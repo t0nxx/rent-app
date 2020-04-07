b63fb210109801e9878e6c72abdefae98cdb3af4
t0nxx <mahmoudko1500@hotmail.com>
2020-04-07 08:52:01 +0200

    Updates

---
+++ precache-manifest.c40c131e024e435de71877b26e40fc36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "574024d95adc6d5e337416eb2911b61c",
+    "url": "/rent-app/index.html"
+  },
+  {
+    "revision": "6c2828624aeba71a4dad",
+    "url": "/rent-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "bff7431aba20ed2b536a",
+    "url": "/rent-app/static/css/main.c8034c6a.chunk.css"
+  },
+  {
+    "revision": "6c2828624aeba71a4dad",
+    "url": "/rent-app/static/js/2.cc5ee787.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/rent-app/static/js/2.cc5ee787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff7431aba20ed2b536a",
+    "url": "/rent-app/static/js/main.28138b02.chunk.js"
+  },
+  {
+    "revision": "99bec97928653e77f050",
+    "url": "/rent-app/static/js/runtime-main.2deb3110.js"
+  }
+]);
